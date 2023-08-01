1453aaf2478fefdb0a33bbd7fef2aed7ef613831
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-01 17:18:00 +0200

    RainbowKit connector

---
+++ frontend/app/components/header.jsx
@@ -0,0 +1,16 @@
+"use client"
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Flex, Text } from "@chakra-ui/react";
+
+const Header = () => {
+    return (
+        <Flex p="2rem" justifyContent="space-between" alignItems="Center">
+            <Text>
+                Logo
+            </Text>
+            <ConnectButton  label="Sign in" showBalance={true} />
+        </Flex>
+    )
+}
+
+export default Header
---
+++ frontend/app/contact/page.js
@@ -0,0 +1,14 @@
+"use client"
+import Header from "../components/header"
+import { Text } from '@chakra-ui/react'
+
+const contact = () => {
+  return (
+    <main>
+      <Header />
+      <Text></Text>
+    </main>
+  )
+}
+
+export default contact
