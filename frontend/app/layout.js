cdbeceef7294f58c12d409ede820b5386acfe71e
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-01 17:37:43 +0200

    Connect local Blockchain

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -8,9 +8,29 @@ import {
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {hardhat, goerli} from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const arpa3Chain = {
+  id: 222222,
+  name: 'Arpa3Chain',
+  network: 'ARPA3BC',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETHEREUM',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://192.168.10.121:8545'],
+    },
+  },
+  testnet: false,
+};
 
 const { chains, publicClient } = configureChains(
-  [hardhat,goerli],[publicProvider()]
+  
+  [hardhat,goerli,arpa3Chain],
+  [jsonRpcProvider({rpc: chain => ({ http: chain.rpcUrls.default.http[0] }),}),]
 );
 
 const { connectors } = getDefaultWallets({
