c00cc5420b80a9df42f98210fd60ccc69ee9a983
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-02 14:27:02 +0200

    Connect local Blockchain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,25 +3,11 @@ const nextConfig = {
     async rewrites() {
         return [
           {
-            source: '//192.168.10.121:8545/:path*',
+            source: '/:path*',
             destination: 'http://192.168.10.121:3000/:path*',
           },
         ]
-      }
-    ,async headers() {
-        return [
-            {
-                source: "/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
-            }
-        ]
-    }, 
-    
+      },
     webpack: (config, { isServer }) => {
         if (!isServer) {
             // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
