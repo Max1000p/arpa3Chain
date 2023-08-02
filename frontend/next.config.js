5ca61c3dbd92cd7f967692171df0d35c176ae5f0
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-02 14:37:52 +0200

    Connect local Blockchain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
           {
             source: '//192.168.10.121:8545/:path*',
-            destination: 'http://192.168.10.121:3000/:path*',
+            destination: 'http://192.168.10.122:3000/:path*',
           },
         ]
       },
