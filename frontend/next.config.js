cf3608bda7dc9523c509b1fb42d5f4e672b6a497
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-02 11:22:25 +0200

    Connect local Blockchain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
-            }
-        ]
-    }, 
     async rewrites() {
         return [
           {
             source: '/:path*',
-            destination: 'http://192.168.10.121:3000/:path*'
+            destination: 'http://192.168.10.121/:path*'
           }
         ]},
         webpack: (config, { isServer }) => {
