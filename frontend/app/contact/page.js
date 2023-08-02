f07b54d42245174b073d63db536cc3e7504019f7
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-02 10:33:02 +0200

    Connect local Blockchain

--- frontend/app/contact/page.js
+++ frontend/app/contact/page.js
@@ -9,23 +9,6 @@ import { readContract,prepareWriteContract, writeContract } from '@wagmi/core'
 import Contract from '../../public/Arpa3.json'
 
 const contact = () => {
-    const http = require('http')
-    const httpServer = http.createServer()
-
-    httpServer.on('request', (request, response) => {
-        // On spécifie l'entête pour le CORS
-        response.setHeader('Access-Control-Allow-Origin', '*');
-
-        // On gère le cas où le navigateur fait un pré-contrôle avec OPTIONS ...
-        // ... pas besoin d'aller plus loin dans le traitement, on renvoie la réponse
-        if (request.method === 'OPTIONS') {
-            // On liste des méthodes et les entêtes valides
-            response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, Authorization')
-            response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-
-            return response.end()
-        }
-    })
     const { isConnected, address : addressAccount } = useAccount()
     const contractAddress = '0x457DaDF045C29C7341431d1A63E1F2747EEfFD11'
 
