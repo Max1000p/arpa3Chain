716ed2def8f1d1687cba4f9cdf5c1b539992feca
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-02 10:56:06 +0200

    Connect local Blockchain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,18 +12,6 @@ const nextConfig = {
                 ]
             }
         ]
-    },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-            config.resolve.fallback = {
-                fs: false,
-                net: false,
-                tls: false
-            }
-        }
-
-        return config;
     }
 
 }
