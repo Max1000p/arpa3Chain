94da4abd6a09e9b909ec07e16011e33f444c7664
Cyril PAREJA <pareja@macbook-pro.home>
2023-08-03 17:34:30 +0200

    Connect local Blockchain

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,9 @@
 "use client"
-import Cors from 'cors'
+
 import Header from "./components/header"
 
 export default function Home() {
-  const cors = Cors({
-    methods: ['POST', 'GET', 'HEAD'],
-  })
-  
+
   return (
     <main>
       <Header />
